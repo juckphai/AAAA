a1debcc2cb66f2722143f0310d4589b6417ebba8
juckphai <llsvuslnw@gmail.com>
2025-10-29 14:49:27 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
-const staticCacheName = 'activity-tracker-static-v38';
-const dynamicCacheName = 'activity-tracker-dynamic-v39';
+const staticCacheName = 'activity-tracker-static-v33';
+const dynamicCacheName = 'activity-tracker-dynamic-v34';
 
 // ไฟล์ที่ต้องการ cache
 const assets = [
